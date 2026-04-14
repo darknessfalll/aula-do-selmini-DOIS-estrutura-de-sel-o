8ac4d518ed0f16f0de2720b603867f547e016d7e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-13 21:51:24 -0300

    triangulo

--- src/treangolo.java
+++ src/treangolo.java
@@ -2,7 +2,30 @@ import java.util.Scanner;
 
 public class treangolo {
     public static void main(String[] args) {
-        Scanner tcld = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" escreva valor do lado do triangulo");
+        double lado1 = sc.nextDouble();
+        System.out.println(" valor atribuido foi: "+ lado1);
+
+        System.out.println(" escreva valor do lado do triangulo");
+        double lado2 = sc.nextDouble();
+        System.out.println(" valor atribuido foi: "+ lado2);
+
+        System.out.println(" escreva valor do lado do triangulo");
+        double lado3 = sc.nextDouble();
+        System.out.println(" valor atribuido foi:" + lado3);
+
+        if ( lado1 <= lado2 + lado3 && lado2 <= lado1 + lado3 && lado3 <= lado1 + lado2){
+            System.out.println(" Isso É TREANGOLO");
+
+        }
+        else{
+            System.out.println(" SE dano erro");
+        }
+
+
+
 
 
     }
